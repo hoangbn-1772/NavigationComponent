1face4ba2d3b45dc8accd98eab6178de6bfdb1a4
bui.ngoc.hoang@framgia.com <buihoangbg01@gmail.com>
2019-06-16 22:33:34 +0700

    Overview

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# NavigationComponent
+﻿# NavigationComponent
+
+## Overview
+
+### Navigation là gì?
+- Navigation nằm trong gói Jetpack, cho phép chuyển màn hình trong app.
+
+### Navigation gồm 3 thành phần
+- Navigation graph: là 1 file XML, chứa tất cả các thông tin màn hình liên quan đến navigation.
+- NavHost: Hiển thị các màn hình điều hướng tương ứng
+- NavController: Đối tượng quản lý điều hướng, hoán đổi màn hình hiển thị trong NavHost
+
+### Một số lợi ích khác
+- Xử lý fragment transactions
+- Xử lý hành động UP và Back chính xác theo mặc định
+- Cung cấp tài nguyên tiêu chuẩn cho animation và transitions.
+- Thực hiện và xử lý deep linking
+- Support: navigation drawer và bottom navigation
+- Support ViewModel
+- Safe Args
+
+### Nguyên tắc
+- Fixed start destination (Điểm bắt đầu): Mỗi app đều có điểm bắt đầu, Đây là màn hình đầu tiên người dùng nhìn thấy khi khởi chạy app, và đó cũng là điểm cuối khi
+người dùng back.
+- Trạng thái Navigation được thể hiện dưới dạng một stack.
+- Up và back
+- Deep linking simulates manual navigation
+
+## Implement
+- Step 1: Tạo navigation graph
+	+ destinations: Là các điểm đến khác nhau trong app.
+	+ actions: Là các kết nối hợp lý giữa các điểm đến.
+
+## Tham khảo
+- https://developer.android.com/guide/navigation/navigation-getting-started#navigate
+- https://medium.com/google-developer-experts/android-navigation-components-part-2-ca643eb301e3
+- https://viblo.asia/p/can-ban-ve-android-navigation-components-E375z0WWZGW
---
+++ app/src/main/java/com/sun/navigationcomponentsample/fragment/HomeFragment.kt
@@ -0,0 +1,51 @@
+package com.sun.navigationcomponentsample.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.sun.navigationcomponentsample.R
+import kotlinx.android.synthetic.main.fragment_home.button_go_detail
+import kotlinx.android.synthetic.main.fragment_home.button_go_search
+
+class HomeFragment : Fragment(), OnClickListener {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initComponents(view)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.button_go_search -> {
+                navController.navigate(R.id.searchFragment)
+            }
+
+            R.id.button_go_detail -> {
+                navController.navigate(R.id.detailFragment)
+            }
+        }
+    }
+
+    private fun initComponents(view: View) {
+        button_go_search?.setOnClickListener(this)
+        button_go_detail?.setOnClickListener(this)
+        navController = Navigation.findNavController(view)
+    }
+
+    // Truyen data
+}
